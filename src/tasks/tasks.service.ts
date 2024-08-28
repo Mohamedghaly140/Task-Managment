bb5a329caaed450b41c6870ec53c47473fed4a7a
mohamedghaly140 <mohamedghaly140@gmail.com>
2024-08-29 01:37:18 +0300

    added dummy data

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { TaskModel } from './task.model';
+import { TaskModel, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks: TaskModel[] = [];
+  private tasks: TaskModel[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'Task 1 description',
+      status: TaskStatus.OPEN,
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+      description: 'Task 2 description',
+      status: TaskStatus.IN_PROGRESS,
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+      description: 'Task 3 description',
+      status: TaskStatus.DONE,
+    },
+  ];
 
   getAllTasks(): TaskModel[] {
     return this.tasks;
