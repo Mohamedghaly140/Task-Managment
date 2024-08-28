fdcf95c10ecc2a078937cc2e89a2413bce6174a0
mohamedghaly140 <mohamedghaly140@gmail.com>
2024-08-29 01:20:46 +0300

    setup proxy server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private http: HttpClient) {}
+
+  fetchTasks() {
+    this.http.get('/api/v1/tasks').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning();
+  app.setGlobalPrefix('api');
   await app.listen(8000);
 }
 bootstrap();
