ad63d643cf18b4165e5f899a8169fbf9e6a5391f
mohamedghaly140 <mohamedghaly140@gmail.com>
2024-09-01 00:38:55 +0300

    added protected route

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TaskEntity } from './task.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskEntity])],
+  imports: [TypeOrmModule.forFeature([TaskEntity]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
